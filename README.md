75a524f5d6863b0fcde8934afda9fcfaed92ce74
ShadowCube264 <68464043+ShadowCube264@users.noreply.github.com>
2023-11-20 13:06:04 +0000

    Add direction_test.py

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Scripts to get data from Nintendo Joycons. Requires [joycon-python](https://pypi
 
 - `joycon_test.py`: Displays basic Joycon input, including buttons pressed and data from the left joystick.
 - `calibration.py`: (WIP) Generates data about Joycon joysticks.
+- `direction_test.py`: A test for getting angles and distance from coordinates. (1800, 1800) is the centre. Requires [getkey](https://pypi.org/project/getkey/).
---
+++ direction_test.py
@@ -0,0 +1,50 @@
+import os, time, getkey, threading, math
+
+x = 1800
+y = 1800
+
+def input_check():
+    global x
+    global y
+
+    while True:
+        key = getkey.getkey()
+        if key == getkey.keys.W and y < 3600:
+            y += 10
+        if key == getkey.keys.S and y > 0:
+            y -= 10
+        if key == getkey.keys.D and x < 3600:
+            x += 10
+        if key == getkey.keys.A and y > 0:
+            x -= 10
+        time.sleep(0.01)
+
+input_thread = threading.Thread(target=input_check, args=())
+input_thread.start()
+
+while True:
+    dX = x - 1800
+    dY = y - 1800
+    distance = math.sqrt(dX**2 + dY**2)
+    inBounds = distance <= 1800
+    if dX == 0 and dY == 0:
+        angle = "N/A"
+    elif dY == 0:
+        if dX > 0:
+            angle = 90
+        else:
+            angle = 270
+    else:
+        angle = round(math.atan(dX/dY)*180/math.pi, 1)
+    
+    if dY < 0:
+        angle += 180
+    elif dX < 0:
+        angle += 360
+
+    os.system("clear")
+    print("x:", x, "\n")
+    print("y:", y, "\n")
+    print("Distance:", round(distance), "  inBounds:", inBounds, "\n")
+    print("Angle:", angle)
+    time.sleep(0.01)
